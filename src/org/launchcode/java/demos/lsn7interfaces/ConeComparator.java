670af062e519b82154fc619ed1230b537b4465b7
nwasserman12 <nickyann0823@aol.com>
2021-05-12 21:46:10 -0500

    Finished exercise

--- src/org/launchcode/java/demos/lsn7interfaces/ConeComparator.java
+++ src/org/launchcode/java/demos/lsn7interfaces/ConeComparator.java
@@ -1,4 +1,17 @@
 package org.launchcode.java.demos.lsn7interfaces;
 
-public class ConeComparator {
+import java.util.Comparator;
+
+public class ConeComparator implements Comparator<Cone> {
+
+    @Override
+    public int compare(Cone o1, Cone o2) {
+        if(o1.getCost() == o2.getCost()){
+            return 0;
+        }else if(o1.getCost() > o2.getCost()){
+            return 1;
+        }else {
+            return -1;
+        }
+    }
 }
--- src/org/launchcode/java/demos/lsn7interfaces/Main.java
+++ src/org/launchcode/java/demos/lsn7interfaces/Main.java
@@ -3,7 +3,6 @@ package org.launchcode.java.demos.lsn7interfaces;
 import java.util.ArrayList;
 
 import java.util.Collections;
-import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args){
@@ -13,20 +12,31 @@ public class Main {
 
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name'
         //  field.
-        System.out.println("Before");
+        System.out.println("Before: \n");
         for (Flavor flavor : flavors){
             System.out.println(flavor.getName());
         }
-        System.out.println("");
 
-        System.out.println("After");
+        System.out.println("After: \n");
         Collections.sort(flavors, new FlavorComparator());
         for (Flavor flavor : flavors){
             System.out.println(flavor.getName());
         }
+
         // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
         //  field.
+        System.out.println("Before: \n");
+        for (Cone cone : cones){
+            System.out.println(cone.getName() + ": " + cone.getCost());
+        }
+
+        System.out.println("After: \n");
+        Collections.sort(cones, new ConeComparator());
+        for (Cone cone : cones){
+            System.out.println(cone.getName() + ": " + cone.getCost());
+        }
 
         // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+
     }
 }
